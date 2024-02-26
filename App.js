cd9d85c66ca013588dbe83cbe8a8bba4f492c00f
tunahantatli <tunahantatl@gmail.com>
2024-02-26 18:06:54 +0300

    first

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-rose-600 font-bold text-[17px]">Hello world!</Text>
+      <TouchableOpacity className="bg-green-500 p-4 rounded-lg mt-4 shadow-xl shadow-black">
+        <Text className="font-semibold text-white">Hey</Text>
+      </TouchableOpacity>
+      <StatusBar style="dark" />
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
